8c9c2acccd40bdafad55520a3a5210ffdddee40e
scross7 <sarahecross7@gmail.com>
2019-11-02 09:55:59 -0500

    updated UFO2 files

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -6,7 +6,7 @@ let tbody = d3.select("tbody");
 // Select the submit button
 const submit = d3.select("#filter-btn");
 
-submit.on("click", function() {
+submit.on("click", function () {
 
   // Prevent the page from refreshing
   d3.event.preventDefault();
@@ -15,40 +15,50 @@ submit.on("click", function() {
   tbody.html("");
 
   // Select the input element and get the raw HTML node
+  const allInput = []
   const date = d3.select("#datetime").property("value");
   const city = d3.select("#city").property("value");
   const state = d3.select("#state").property("value");
   const country = d3.select("#country").property("value");
   const shape = d3.select("#shape").property("value");
 
-  const allInput = [date, city, state, country, shape]
-  const input = []
-  
-  for (allInput) {
-    if (allInput != "") {
-      input.push(allInput)
-    };
-  };
+  // Filter data by non-null input values only
+  const filteredData = tableData
+    .filter(obj => {
+      return date === "" || obj.datetime === date;
+    })
+    .filter(obj => {
+      return city === "" || obj.city === city;
+    })
+    .filter(obj => {
+      return state === "" || obj.state === state;
+    })
+    .filter(obj => {
+      return country === "" || obj.country === country;
+    })
+    .filter(obj => {
+      return shape === "" || obj.shape === shape;
+    })
 
-  console.log(input)
-
-  // Get the value property of the input element
-  const filteredData = tableData.filter(day => 
-    day.datetime === date
-    && day.city === city
-    && day.state === state
-    && day.country === country
-    && day.shape === shape
-    );
-  
-  // console.log(filteredData);
-
-  filteredData.forEach(ufo => {
+  // If all input values are null, return no results message
+  if (date === "" && city === "" && state === "" && country === "" && shape === "") {
     const row = tbody.append("tr");
-    for (key in ufo){
+    const cell = tbody.append("td");
+    cell.text("your search did not return any results");
+  }
+
+  // Otherwise, display filtered data
+  else {
+    filteredData.forEach(ufo => {
+      const row = tbody.append("tr");
+      for (key in ufo) {
         const cell = tbody.append("td");
-        cell.text(ufo[key]);
-    }; 
-});
+        cell.text(ufo[key])
+      };
+
+    });
+    };
+
+
 
 });
